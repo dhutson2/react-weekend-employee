6deae1c0a075e784d51ce12e08c0e809c6388eba
Derek Hutson <denverd1301@gmail.com>
2019-08-09 15:56:48 -0600

    added create employee logic, appended to employees container

--- src/CreateEmployee/index.js
+++ src/CreateEmployee/index.js
@@ -3,10 +3,70 @@ import React, { Component } from "react";
 class Employee extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "",
+      position: "",
+      birthDate: "",
+      department: "",
+      annualSalary: ""
+    };
   }
+
+  updateEmployee = e => {
+    this.setState({ [e.currentTarget.name]: [e.currentTarget.value] });
+  };
+
   render() {
-    return <div>create me!</div>;
+    return (
+      <form onSubmit={this.props.addEmployee.bind(null, this.state)}>
+        <label htmlFor="">
+          Employee Name:
+          <input
+            type="text"
+            name="name"
+            onChange={this.updateEmployee}
+            value={this.state.name}
+          />
+        </label>
+        <label htmlFor="">
+          Position:
+          <input
+            type="text"
+            name="position"
+            onChange={this.updateEmployee}
+            value={this.state.position}
+          />
+        </label>
+        <label htmlFor="">
+          Birthday:
+          <input
+            type="text"
+            name="birthDate"
+            onChange={this.updateEmployee}
+            value={this.state.birthDate}
+          />
+        </label>
+        <label htmlFor="">
+          Department:
+          <input
+            type="text"
+            name="department"
+            onChange={this.updateEmployee}
+            value={this.state.department}
+          />
+        </label>
+        <label htmlFor="">
+          Salary:
+          <input
+            type="text"
+            name="annualSalary"
+            onChange={this.updateEmployee}
+            value={this.state.annualSalary}
+          />
+        </label>
+        <button type="submit">Hire Employee</button>
+      </form>
+    );
   }
 }
 
--- src/EmployeesContainer/index.js
+++ src/EmployeesContainer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Employee from "../CreateEmployee";
+import CreateEmployee from "../CreateEmployee";
 
 class Employees extends Component {
   constructor() {
@@ -18,8 +18,37 @@ class Employees extends Component {
     };
   }
 
+  addEmployee = async (employee, e) => {
+    e.preventDefault();
+    console.log(employee, "inside of add employee");
+    try {
+      const createEmployee = await fetch(
+        "http://localhost:9000/api/v1/employee",
+        {
+          method: "POST",
+          credentials: "include",
+          body: JSON.stringify(employee),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      // if (CreateEmployee.message !== "Success") {
+      //   throw Error("WTF? Resource not found");
+      // }
+      const createEmployeeResponse = await createEmployee.json();
+      console.log(createEmployeeResponse, "Create employee response");
+      this.setState({
+        employees: [...this.state.employees, createEmployeeResponse.data]
+      });
+    } catch (err) {
+      console.log(err, "add employee error");
+      return err;
+    }
+  };
+
   render() {
-    return <Employee />;
+    return <CreateEmployee addEmployee={this.addEmployee} />;
   }
 }
 
