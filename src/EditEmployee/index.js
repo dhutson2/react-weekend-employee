2b99b0c77887c35848278badbe16f5c067d1a41c
Derek Hutson <denverd1301@gmail.com>
2019-08-12 09:15:40 -0600

    more refactoring

--- src/EditEmployee/index.js
+++ src/EditEmployee/index.js
@@ -1,39 +1,65 @@
-import React, { Component } from 'react';
-import {Form, Button, Label} from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Form, Button, Label } from "semantic-ui-react";
 
-const EditMovie = (props) =>  {
-
+const EditEmployee = props => {
   return (
     <div>
       <h3> Edit Employee</h3>
       <Form onSubmit={props.closeAndEdit}>
         <Label>
           Edit Employee Name:
-          <Form.Input type="text" name="name" onChange={props.handleFormChange} value={props.employeeToEdit.name}/>
-        </Label><br/>
+          <Form.Input
+            type="text"
+            name="name"
+            onChange={props.handleFormChange}
+            value={props.employeeToEdit.name}
+          />
+        </Label>
+        <br />
         <Label>
           Edit Employee Position:
-          <Form.Input type="text" name="position" onChange={props.handleFormChange} value={props.employeeToEdit.position}/>
-        </Label><br/>
+          <Form.Input
+            type="text"
+            name="position"
+            onChange={props.handleFormChange}
+            value={props.employeeToEdit.position}
+          />
+        </Label>
+        <br />
         <Label>
           Edit Employee BirthDate:
-          <Form.Input type="date" name="birthDate" onChange={props.handleFormChange} value={props.employeeToEdit.birthDate}/>
-        </Label><br/>
+          <Form.Input
+            type="date"
+            name="birthDate"
+            onChange={props.handleFormChange}
+            value={props.employeeToEdit.birthDate}
+          />
+        </Label>
+        <br />
         <Label>
           Edit Employee Department:
-          <Form.Input type="text" name="department" onChange={props.handleFormChange} value={props.employeeToEdit.department}/>
-        </Label><br/>
+          <Form.Input
+            type="text"
+            name="department"
+            onChange={props.handleFormChange}
+            value={props.employeeToEdit.department}
+          />
+        </Label>
+        <br />
         <Label>
           Edit Annual Salary:
-          <Form.Input type="text" name="annualSalary" onChange={props.handleFormChange} value={props.movieToEdit.annualSalary}/>
-        </Label><br/>
-        <Button type='Submit'>Edit</Button>
+          <Form.Input
+            type="text"
+            name="annualSalary"
+            onChange={props.handleFormChange}
+            value={props.movieToEdit.annualSalary}
+          />
+        </Label>
+        <br />
+        <Button type="Submit">Edit</Button>
       </Form>
     </div>
-
-    )
-  }
+  );
+};
 
 export default EditEmployee;
-
- 
